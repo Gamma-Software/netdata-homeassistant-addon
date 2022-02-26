21a297e2f75289daa31a8d8b79affa2fb9a4b4a5
Valentin <valentin.rudloff.perso@gmail.com>
2022-02-26 22:05:55 +0100

    update shell

--- netdata/rootfs/usr/bin/run.sh
+++ netdata/rootfs/usr/bin/run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/with-contenv bashio
-
 bashio::log.info "Configuring Netdata..."
 /usr/bin/config_netdata.sh
-
 bashio::log.info "Starting Netdata..."
 exec /opt/netdata/bin/netdata -p 19999 -D -c /etc/netdata/netdata.conf
